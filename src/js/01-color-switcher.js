a0e5dc26d32df04dc8bddbe39440dd780c70cca2
AndriiKalinchuk <126283550+AndriiKalinchuk@users.noreply.github.com>
2023-05-04 16:15:12 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const CHANGE_COLOR_DELAY = 1000;
+let idInt = null;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStartBtnChangeColor);
+stopBtn.addEventListener('click', onStopBtnChangeColor);
+
+function onStartBtnChangeColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  idInt = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onStopBtnChangeColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
